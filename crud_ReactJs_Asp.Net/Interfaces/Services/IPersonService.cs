df8b4b4873814f442fd8ff1573fa1d95fed071d6
Bruno Barbosa <brunobm19992014@gmail.com>
2022-11-15 19:40:36 +0000

    IPersonService and PersonService
    Interface and Service created to declare and implement the functions that will communicate to the services responsible to communicates with the database and give results to the user.
    
    For now, it is only implemented the function "GetAllPersons" and "AddPerson"

---
+++ crud_ReactJs_Asp.Net/Interfaces/Services/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using crud_ReactJs_Asp.Net.Entities;
+
+namespace crud_ReactJs_Asp.Net.Interfaces.Services {
+    public interface IPersonService {
+        /* This interface was created with an objective to declare the functions that will be needed to response the request from the user and to conect to the interface that make
+         * the connection to the database.
+         * In a simple way, the functions here declared will be used in PersonService.cs where are implemented and with the injection of the interface responsible to the database connection give the user some response.
+         */
+        Task<Person> AddPerson(Person person);
+        Task<Person> UpdatePerson(Person person);
+        Task<bool> DeletePerson(Person person);
+        Task<Person[]> GetAllPersons();
+        Task<Person[]> GetPersonsyName(string personName);
+        Task<Person> GetPersonById(Guid personId);
+        Task<Person> GetPersonByNifAsync(string personNIF);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
