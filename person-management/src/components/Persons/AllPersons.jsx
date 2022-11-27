ff4c3f0e1b8ef5a340c42109947e6e25cfb53b9a
Bruno Barbosa <brunobm19992014@gmail.com>
2022-11-27 01:04:06 +0000

    Loading state when getting persons

--- person-management/src/components/Persons/AllPersons.jsx
+++ person-management/src/components/Persons/AllPersons.jsx
@@ -5,30 +5,32 @@ import GoBackArrow from "../Shared/GoBackArrow";
 import style from "./../../css/AllPersons.module.css";
 import Swal from "sweetalert2";
 
-
 const AllPersons = (props) => {
-
   const [initialPersons, setInitialPersons] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   const [searchText, setSearchText] = useState("");
 
   const GetAllPersons = () => {
-    props.personAPI().fetchAll().then(res => {
-      setInitialPersons(res.data);
-    });
-  }
+    setLoading(true);
+    props
+      .personAPI()
+      .fetchAll()
+      .then((res) => {
+        setInitialPersons(res.data);
+      });
+      setLoading(false);
+  };
   useEffect(() => {
-      GetAllPersons();
-  }, [])
+    GetAllPersons();
+  }, []);
 
   const OnHandlerSearchText = (searchText) => {
     setSearchText(searchText);
   };
 
-
   const deleteHandler = (data) => {
     //"data" is the person that he have to pass in the body request to the api call to delete the user from the database
-    if(data.id !== 0){
+    if (data.id !== 0) {
       Swal.fire({
         title: "Are you sure you want to delete this person?",
         text: "If deleted you won’t be able to recover it!",
@@ -39,14 +41,21 @@ const AllPersons = (props) => {
         confirmButtonText: "Delete",
       }).then((result) => {
         if (result.isConfirmed) {
-          props.personAPI().delete(data.id).then(res => {
-            if(res.data){
-              Swal.fire("Deleted!", "Person deleted with sucess!", "success");
-              GetAllPersons();
-            }else{
-              Swal.fire("Error!", "Unexpected error while trying to delete a person!", "error");
-            }
-          });
+          props
+            .personAPI()
+            .delete(data.id)
+            .then((res) => {
+              if (res.data) {
+                Swal.fire("Deleted!", "Person deleted with sucess!", "success");
+                GetAllPersons();
+              } else {
+                Swal.fire(
+                  "Error!",
+                  "Unexpected error while trying to delete a person!",
+                  "error"
+                );
+              }
+            });
         }
       });
     }
@@ -54,23 +63,57 @@ const AllPersons = (props) => {
 
   const viewHandler = (person) => {
     props.personView(person);
-  }
-  const editHandler = (id) => {
-    props.personEdit(id);
-  }
+  };
+  const editHandler = (person) => {
+    props.personEdit(person);
+  };
 
+  let content = "";
+  if (loading) {
+    content = (
+      <div className="col-12 d-flex justify-content-center">
+        <div className="loader"></div>
+      </div>
+    );
+  } else if (
+    initialPersons.filter((person) =>
+      person.firstName
+        .toLocaleLowerCase()
+        .includes(searchText.toLocaleLowerCase())
+    ).length === 0
+  ) {
+    content = (
+      <h1
+        className="d-flex justify-content-center"
+        style={{ color: "#2f6e82" }}
+      >
+        Resultados não encontrados
+      </h1>
+    );
+  } else {
+    content = initialPersons
+      .filter((person) =>
+        person.firstName
+          .toLocaleLowerCase()
+          .includes(searchText.toLocaleLowerCase())
+      )
+      .map((person, index) => {
+        return (
+          <PersonCard
+            key={person.id}
+            onHandlerView={viewHandler}
+            onHandlerDeleteInput={deleteHandler}
+            personDetails={person}
+            editHandler={editHandler}
+          />
+        );
+      });
+  }
   return (
     <>
       <GoBackArrow location={"allPersons"} allPersons={props.GoBack} />
       <SearchbarInput OnHandlerSearchText={OnHandlerSearchText} />
-      <div className={`row ${style.personContainer} scrollbar`}>
-        {initialPersons.filter(person => person.firstName.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())).length === 0 && <h1 className="d-flex justify-content-center" style={{color: "#2f6e82"}}>Resultados não encontrados</h1>}
-        {initialPersons.filter(person => person.firstName.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())).map((person, index) => {
-          return(
-            <PersonCard key={person.id} onHandlerView={viewHandler} onHandlerDeleteInput={deleteHandler} personDetails={person} editHandler={editHandler}/>
-          )
-        })}
-      </div>
+      <div className={`row ${style.personContainer} scrollbar`}>{content}</div>
     </>
   );
 };
